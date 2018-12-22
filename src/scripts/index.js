56637adce0080804557595b3e553aa22ff9be920
Kangyun Wang <kevinwang8011@hotmail.com>
2018-12-22 22:07:14 +0100

    Resume (start) analyser on click

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,45 +2,53 @@ import '../styles/index.css';
 
 import pitchAnalyser from 'pitch-analyser';
 
-// Get elements from page
-const aNote = document.getElementById('a-note');
-const aFrequency = document.getElementById('a-frequency');
-const aCents = document.getElementById('a-cents');
+window.onload = function() {
+	// Get elements from page
+	const aNote = document.getElementById('a-note');
+	const aFrequency = document.getElementById('a-frequency');
+	const aCents = document.getElementById('a-cents');
+	const startButton = document.querySelector('main > div:first-of-type button');
 
-// Analyser options
-const analyserOptions = {
-	callback: analyserCallback,
-	returnNote: true,
-	returnCents: true,
-	decimals: 2,
+	// Analyser options
+	const analyserOptions = {
+		callback: analyserCallback,
+		returnNote: true,
+		returnCents: true,
+		decimals: 2,
+	};
+
+	// Initialize the analyser
+	const analyser = new pitchAnalyser(analyserOptions);
+
+	// What to do with the audio payload
+	function analyserCallback(payload) {
+		updateNote(payload.note);
+		updateFrequency(payload.frequency);
+		updateCents(payload.cents);
+	}
+
+	// Update the note on the page
+	function updateNote(note) {
+		aNote.innerText = note;
+	}
+
+	// Update the frequency on the page
+	function updateFrequency(frequency) {
+		aFrequency.innerText = frequency;
+	}
+
+	// Update the cents on the page (Not sure about the accuracy)
+	function updateCents(cents) {
+		aCents.innerText = cents;
+	}
+
+	// Handle error
+	function handleError(err) {
+		throw new Error(`Opps something went wrong: ${err}`);
+	}
+
+	startButton.addEventListener('click', function() {
+		analyser.audioContext.resume();
+		startButton.classList.add('hidden');
+	});
 };
-
-// Initialize the analyser
-const analyser = new pitchAnalyser(analyserOptions);
-
-// What to do with the audio payload
-function analyserCallback(payload) {
-	updateNote(payload.note);
-	updateFrequency(payload.frequency);
-	updateCents(payload.cents);
-}
-
-// Update the note on the page
-function updateNote(note) {
-	aNote.innerText = note;
-}
-
-// Update the frequency on the page
-function updateFrequency(frequency) {
-	aFrequency.innerText = frequency;
-}
-
-// Update the cents on the page (Not sure about the accuracy)
-function updateCents(cents) {
-	aCents.innerText = cents;
-}
-
-// Handle error
-function handleError(err) {
-	throw new Error(`Opps something went wrong: ${err}`);
-}
